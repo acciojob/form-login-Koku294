adbd9c983916649c02a024fbf83170677cb1d945
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-03-05 22:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let Firstname = document.getElementById("fname").value;
+	let Lastname = document.getElementById("lname").value;
+	alert(`${Firstname}${Lastname}`);
 
 }
